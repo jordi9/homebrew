281b2ea1d0f0cdf244ed74ec89bd09a7727fc015
Jordi Gerona <jordi9@trovit.com>
2013-07-18 11:13:00 +0200

    rtm: Added homepage

--- Library/Formula/rtm.rb
+++ Library/Formula/rtm.rb
@@ -1,10 +1,7 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Rtm < Formula
-  homepage ''
+  homepage 'http://www.davidwaring.net/projects/rtm.html'
   url 'https://bitbucket.org/dwaring87/rtm-cli/get/tip.tar.gz'
   sha1 'c6ceb992a4a2b0eccc8db1ff807996e03bc3b46f'
   version '1.3.2'
@@ -12,12 +9,6 @@ class Rtm < Formula
   depends_on :python => ['rtm' => 'pyrtm']
 
   def install
-    # ENV.j1  # if your formula's build system can't parallelize
-
-    # system "./configure", "--disable-debug", "--disable-dependency-tracking",
-    #                     "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    # system "make", "install" # if this fails, try separate make/make install steps
     bin.install 'rtm'
   end
 
@@ -27,6 +18,6 @@ class Rtm < Formula
     # This test will fail and we won't accept that! It's enough to just replace
     # "false" with the main program this formula installs, but it'd be nice if you
     # were more thorough. Run the test with `brew test rtm`.
-    system "false"
+    system rtm
   end
 end
